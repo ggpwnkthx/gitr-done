2d749bd99be8d42a2684f5d485124cdb20ed9c18
ggpwnkthx <ibjessup@gmail.com>
2020-06-06 14:56:10 -0400

    added fuse

--- run.sh
+++ run.sh
@@ -56,7 +56,7 @@ set_sh_c() {
 
 install_prerequisites() {
 	# Run setup for each distro accordingly
-	packages="sudo git curl"
+	packages="sudo git curl fuse"
 	case "$pkgmgr" in
 		apt|apt-get)
 			packages="apt-transport-https ca-certificates $packages"
