54702a8bc8b9754ef7d6155a291d6054d768d399
ggpwnkthx <ibjessup@gmail.com>
2020-06-14 09:45:55 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -55,7 +55,7 @@ run_privileged() {
 	if [ "$user" != 'root' ]; then
 		echo "Not running as a privileged user. Attempting to restart with authority..."
 		run="$0 $user $@"
-		if [ command_exists sudo ] && [ ! -z "$(groups $(whoami) | tr " " "\n" | grep '^sudo$')" ]; then
+		if command_exists sudo && [ ! -z "$(groups $(whoami) | tr " " "\n" | grep '^sudo$')" ]; then
 			(
 				set -x
 				sudo -E "$run"
